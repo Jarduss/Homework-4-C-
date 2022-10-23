47d9aa333e61cf149921204211bbbf608883dc70
danil <jardus@bk.ru>
2022-10-23 15:13:00 +0300

    Homework_4

---
+++ Задача 2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum(int num)
+{
+    int count = Convert.ToString(num).Length;
+    int last_num = 0;
+    int result = 0;
+    for (int i = 0; i< count; i++)
+    {
+        last_num = num - num % 10;
+        result = result + (num - last_num);
+        num = num/10;
+    }
+     return result;
+}
+int summ = sum(num);
+Console.WriteLine("Сумма цифр в числе: " + summ);
---
+++ Задача 3/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.Write("Введите ряд чисел, разделенных запятой, без пробелов : ");
+string seriesOfNumbers = Console.ReadLine();
+seriesOfNumbers = seriesOfNumbers + ",";   
+
+int[] ArrayOfNumbers(string seriesOfNumbers)
+{ 
+  int[] arrayOfNumbers = new int[1];    
+  int j =0;
+  for (int i = 0; i < seriesOfNumbers.Length; i++)
+  {
+    string seriesNew1 = string.Empty;
+    while (seriesOfNumbers[i] != ',' && i < seriesOfNumbers.Length){
+      seriesNew1 += seriesOfNumbers[i];
+      i++;
+    }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    
+    if (i < seriesOfNumbers.Length-1){
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count){
+    Console.Write(coll[index]);
+    index++;
+    if (index < count){
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesOfNumbers);
+PrintArry(arrayOfNumbers);
